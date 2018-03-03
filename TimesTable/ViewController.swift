4de2f959ee2df9a47db4bc946566838f1ef6211a
parthlmb1 <parthlmb007@gmail.com>
2018-03-03 16:13:28 +0530

    full project

--- TimesTable/ViewController.swift
+++ TimesTable/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+    @IBOutlet weak var sliderValue: UISlider!
+    @IBOutlet weak var teble: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,28 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func sliderMoved(_ sender: AnyObject) {
+        //print(sliderValue.value)
+        teble.reloadData()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAtIndexPath indexPath: IndexPath) -> UITableViewCell{
+    
+    
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        let timesTable = Int(sliderValue.value * 20 )
+        
+        cell.textLabel?.text = String(timesTable * (indexPath.row + 1))
+        
+        return cell
+        
+    
     }
 
 
